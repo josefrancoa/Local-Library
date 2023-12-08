e4486e34e54141e37f1f838862e47102ae59de26
José <80324714+josefrancoa@users.noreply.github.com>
2023-12-08 16:41:09 +0100

    Update authorController.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -45,15 +45,15 @@ exports.author_create_post = [
     .isLength({ min: 1 })
     .escape()
     .withMessage("First name must be specified.")
-    .isAlphanumeric()
-    .withMessage("First name has non-alphanumeric characters."),
+    .matches(/^[A-Za-z ]+$/)
+    .withMessage("First name must contain only letters and spaces."),
   body("family_name")
     .trim()
     .isLength({ min: 1 })
     .escape()
     .withMessage("Family name must be specified.")
-    .isAlphanumeric()
-    .withMessage("Family name has non-alphanumeric characters."),
+    .matches(/^[A-Za-z ]+$/)
+    .withMessage("Family name must contain only letters and spaces."),
   body("date_of_birth", "Invalid date of birth")
     .optional({ values: "falsy" })
     .isISO8601()
@@ -138,30 +138,51 @@ exports.author_update_get = asyncHandler(async (req, res, next) => {
   res.render("author_form", { title: "Update Author", author: author });
 });
 
-exports.author_update_post = asyncHandler(async (req, res, next) => {
-  const errors = validationResult(req);
+exports.author_update_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .matches(/^[A-Za-z ]+$/)
+    .withMessage("First name must contain only letters and spaces."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .matches(/^[A-Za-z ]+$/)
+    .withMessage("Family name must contain only letters and spaces."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
 
-  const author = new Author({
-    _id: req.params.id,
-    first_name: req.body.first_name,
-    family_name: req.body.family_name,
-    date_of_birth: req.body.date_of_birth,
-    date_of_death: req.body.date_of_death,
-  });
-
-  if (!errors.isEmpty()) {
-    res.render("author_form", {
-      title: "Update Author",
-      author: author,
-      errors: errors.array(),
+    const author = new Author({
+      _id: req.params.id,
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
     });
-    return;
-  } else {
-    const updatedAuthor = await Author.findByIdAndUpdate(
-      req.params.id,
-      author,
-      {}
-    );
-    res.redirect(updatedAuthor.url);
-  }
-});
+
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Update Author",
+        author: author,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await Author.findByIdAndUpdate(req.params.id, author);
+      res.redirect(author.url);
+    }
+  }),
+];
